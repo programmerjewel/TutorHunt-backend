82ef38bb313e7ffefc38267e1cc951470d11b425
programmerjewel <jewelruet1402@gmail.com>
2025-07-16 12:35:56 +0600

    initial mongodb, dotenvx setup

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors')
+const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb')
+require('@dotenvx/dotenvx').config() 
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mujla7p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+   const tutorCollection = client.db('tutorsdb').collection('tutors');
+   const bookedTutorCollection = client.db('tutorsdb').collection('booked-tutors');
+  
+    
+
+   
+
+    
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from TutorHunt Server....');
+});
+
+app.listen(
+  port, () => console.log(`Server running on port ${port}`)
+);
